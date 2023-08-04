8cb0043cdab806d4383a74cd35cd42cee7a07088
jananikangesan <janakanges1996@gmail.com>
2023-08-04 12:01:09 +0530

    Feign Client with Eureka Server

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 public class RestapiApplication implements CommandLineRunner {
 
